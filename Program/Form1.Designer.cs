631185b6878956aa9e0756cde6fddfffbc39f05b
rostikсheremys <rostikcheremys@gmail.com>
2024-04-08 22:08:03 +0300

    Initial commit

---
+++ Program/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Program
+{
+    sealed partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MultiThread = new System.Windows.Forms.CheckBox();
+            this.btnRandomPoints = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MultiThread
+            // 
+            this.MultiThread.AutoSize = true;
+            this.MultiThread.Location = new System.Drawing.Point(12, 16);
+            this.MultiThread.Name = "MultiThread";
+            this.MultiThread.Size = new System.Drawing.Size(85, 17);
+            this.MultiThread.TabIndex = 0;
+            this.MultiThread.Text = "Multi-Thread";
+            this.MultiThread.UseVisualStyleBackColor = true;
+            this.MultiThread.CheckedChanged += new System.EventHandler(this.SingleThread_CheckedChanged);
+            // 
+            // btnRandomPoints
+            // 
+            this.btnRandomPoints.Location = new System.Drawing.Point(557, 12);
+            this.btnRandomPoints.Name = "btnRandomPoints";
+            this.btnRandomPoints.Size = new System.Drawing.Size(115, 23);
+            this.btnRandomPoints.TabIndex = 1;
+            this.btnRandomPoints.Text = "Random Points";
+            this.btnRandomPoints.UseVisualStyleBackColor = true;
+            this.btnRandomPoints.Click += new System.EventHandler(this.btnRandomPoints_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.btnRandomPoints);
+            this.Controls.Add(this.MultiThread);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainForm_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.CheckBox MultiThread;
+        private System.Windows.Forms.Button btnRandomPoints;
+
+        #endregion
+    }
+}
---
+++ Program/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program
+{
+    public sealed partial class Form1 : Form
+    {
+        private readonly List<Point> _points = new();
+        private bool _singleThreadMode = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Малювання вершин
+            foreach (Point p in _points)
+            {
+                g.FillEllipse(Brushes.Black, p.X - 3, p.Y - 3, 8, 8);
+            }
+
+            // Побудова діаграми Вороного
+            if (_points.Count >= 2)
+            {
+                if (_singleThreadMode)
+                    DrawVoronoiDiagramSingleThread(g);
+                else
+                    DrawVoronoiDiagramMultiThread(g);
+            }
+        }
+
+        // Додавання вершини за допомогою кліка миші
+        // Додавання або видалення вершини за допомогою кліка миші
+        private void MainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point clickedPoint = e.Location;
+                bool pointRemoved = false;
+
+                // Перевіряємо, чи клікнуто біля існуючої вершини
+                for (int i = 0; i < _points.Count; i++)
+                {
+                    if (Distance(clickedPoint.X, clickedPoint.Y, _points[i]) <= 5) // Перевірка на відстань від кліку до вершини
+                    {
+                        _points.RemoveAt(i);
+                        pointRemoved = true;
+                        break;
+                    }
+                }
+
+                // Якщо вершину не видалили, то додаємо нову
+                if (!pointRemoved)
+                {
+                    _points.Add(clickedPoint);
+                }
+
+                Invalidate();
+            }
+        }
+
+
+        // Генерація випадкових вершин
+        private void btnRandomPoints_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int numPoints = rand.Next(5, 15);
+
+            _points.Clear();
+            for (int i = 0; i < numPoints; i++)
+            {
+                _points.Add(new Point(rand.Next(ClientSize.Width), rand.Next(ClientSize.Height)));
+            }
+
+            Invalidate();
+        }
+
+        // Вибір режиму обчислень: однопоточний або багатопоточний
+        private void SingleThread_CheckedChanged(object sender, EventArgs e)
+        {
+            _singleThreadMode = MultiThread.Checked;
+            Invalidate();
+        }
+
+        // Побудова діаграми Вороного в однопоточному режимі
+        private void DrawVoronoiDiagramSingleThread(Graphics g)
+        {
+            for (int x = 0; x < ClientSize.Width; x++)
+            {
+                for (int y = 0; y < ClientSize.Height; y++)
+                {
+                    Point closestPoint = _points[0];
+                    double minDistance = Distance(x, y, closestPoint);
+
+                    for (int i = 1; i < _points.Count; i++)
+                    {
+                        double dist = Distance(x, y, _points[i]);
+                        if (dist < minDistance)
+                        {
+                            minDistance = dist;
+                            closestPoint = _points[i];
+                        }
+                    }
+
+                    g.FillRectangle(new SolidBrush(Color.FromArgb(100, closestPoint.X % 256, closestPoint.Y % 256, 255)), x, y, 1, 1);
+                }
+            }
+        }
+
+        // Побудова діаграми Вороного в багатопоточному режимі
+        private void DrawVoronoiDiagramMultiThread(Graphics g)
+        {
+            const int numThreads = 4;
+            
+            List<Task> tasks = new List<Task>();
+
+            for (int t = 0; t < numThreads; t++)
+            {
+                int threadId = t;
+                
+                Task task = Task.Run(() =>
+                {
+                    for (int x = threadId; x < ClientSize.Width; x += numThreads)
+                    {
+                        for (int y = 0; y < ClientSize.Height; y++)
+                        {
+                            Point closestPoint = _points[0];
+                            double minDistance = Distance(x, y, closestPoint);
+
+                            for (int i = 1; i < _points.Count; i++)
+                            {
+                                double dist = Distance(x, y, _points[i]);
+                                if (dist < minDistance)
+                                {
+                                    minDistance = dist;
+                                    closestPoint = _points[i];
+                                }
+                            }
+
+                            lock (g)
+                            {
+                                g.FillRectangle(new SolidBrush(Color.FromArgb(100, closestPoint.X % 256, closestPoint.Y % 256, 255)), x, y, 1, 1);
+                            }
+                        }
+                    }
+                });
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        // Обчислення відстані між двома точками
+        private double Distance(int x1, int y1, Point p2)
+        {
+            int dx = x1 - p2.X;
+            int dy = y1 - p2.Y;
+            
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+    }
+}
